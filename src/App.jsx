d938adf108014591ef68ca58efafb812bb5fe0d7
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-09-19 16:32:23 -0700

    Started working on NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <div>hi</div>
+      <NavBar />
     </>
   );
 }
