abecf2189484af95bfb0d3c0435bbecf936272dc
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-09-22 14:18:00 -0700

    Moved content of HomePage into a separate component
    Created the HeroComponent

---
+++ src/components/HeroComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import MainPic from "../assets/manExercising.png";
+import { Link } from "react-router-dom";
+
+const HeroComponent = () => {
+  return (
+    <div className="heroComponent">
+      <div className="homePageHook">
+        <p className="brandName">Xercise</p>
+        <p>Your Online Exercise Companion</p>
+        <Link to="exercises" className="exploreBtn">
+          Explore Exercises
+        </Link>
+      </div>
+      <img src={MainPic} alt="man exercising with dumbbells (Renegade row)" />
+      <p className="backSlogan">Unleash your Inner Beast</p>
+    </div>
+  );
+};
+
+export default HeroComponent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,11 @@
 import React from "react";
-import MainPic from "../assets/manExercising.png";
-import { Link } from "react-router-dom";
+import HeroComponent from "../components/HeroComponent";
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="homePage">
-      <div className="homePageHook">
-        <p className="brandName">Xercise</p>
-        <p>Your Online Exercise Companion</p>
-        <Link to="exercises" className="exploreBtn">
-          Explore Exercises
-        </Link>
-      </div>
-      <img src={MainPic} alt="man exercising with dumbbells (Renegade row)" />
-      <p className="backSlogan">Unleash your Inner Beast</p>
+      <HeroComponent />
     </div>
   );
 };
