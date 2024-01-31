a0edcfba12d1ad6950e57598ff29d96c3e55b85d
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-01-30 22:16:26 -0800

    Started implementing search functionality

--- src/components/ExerciseSearch.jsx
+++ src/components/ExerciseSearch.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const ExerciseSearch = () => {
+  const [textInput, setTextInput] = useState("");
+  const [exercises, setExercises] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchBodyParts = async () => {
+      const bodyParts = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+
+      setBodyParts([...bodyParts]);
+    };
+
+    fetchBodyParts();
+  }, []);
+
+  const handleSearch = async () => {
+    if (textInput) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises?limit=-1",
+        exerciseOptions
+      );
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(textInput) ||
+          exercise.target.toLowerCase().includes(textInput) ||
+          exercise.equipment.toLowerCase().includes(textInput) ||
+          exercise.bodyPart.toLowerCase().includes(textInput)
+      );
+
+      setTextInput("");
+      setExercises(searchedExercises);
+    }
+  };
+
   return (
     <div className="searchContainer">
-      <input type="text" placeholder="Search Exercise" />
-      <button>Search</button>
+      <input
+        type="text"
+        placeholder="Search Exercise"
+        onChange={(e) => setTextInput(e.target.value.toLowerCase())}
+      />
+      <button onClick={handleSearch}>Search</button>
     </div>
   );
 };
