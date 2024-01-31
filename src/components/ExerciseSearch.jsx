89a7f61122f985fc07f4d8806cf11c330db4b451
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-01-30 20:34:39 -0800

    Setting up ExerciseSearch container

---
+++ src/components/ExerciseSearch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ExerciseSearch = () => {
+  return (
+    <div className="searchContainer">
+      <input type="text" placeholder="Search Exercise" />
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default ExerciseSearch;
