7183253468a444640fcc0510b7fb118e85149d65
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-01-31 21:45:22 -0800

    creating horizontalScrollBar for body parts

--- src/components/ExerciseSearch.jsx
+++ src/components/ExerciseSearch.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollBar from "./HorizontalScrollBar";
 
 const ExerciseSearch = () => {
   const [textInput, setTextInput] = useState("");
-  const [exercises, setExercises] = useState("");
+  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
@@ -21,6 +22,7 @@ const ExerciseSearch = () => {
   }, []);
 
   const handleSearch = async () => {
+    console.log(bodyParts);
     if (textInput) {
       const exercisesData = await fetchData(
         "https://exercisedb.p.rapidapi.com/exercises?limit=-1",
@@ -41,14 +43,17 @@ const ExerciseSearch = () => {
   };
 
   return (
-    <div className="searchContainer">
-      <input
-        type="text"
-        placeholder="Search Exercise"
-        onChange={(e) => setTextInput(e.target.value.toLowerCase())}
-      />
-      <button onClick={handleSearch}>Search</button>
-    </div>
+    <>
+      <div className="searchContainer">
+        <input
+          type="text"
+          placeholder="Search Exercise"
+          onChange={(e) => setTextInput(e.target.value.toLowerCase())}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <HorizontalScrollBar data={bodyParts} />
+    </>
   );
 };
 
