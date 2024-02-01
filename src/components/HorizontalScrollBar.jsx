4a66fb0c200d1e7ed637c8fa54821228ebb37939
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-01-31 23:20:44 -0800

    Added arrows and the ScrollMenu

--- src/components/HorizontalScrollBar.jsx
+++ src/components/HorizontalScrollBar.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import BodyPart from "./BodyPart";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import "react-horizontal-scrolling-menu/dist/styles.css";
+import LeftArrow from "../assets/leftArrow.png";
+import rightArrow from "../assets/rightArrow.png";
 
 const HorizontalScrollBar = ({
   data,
@@ -7,15 +11,19 @@ const HorizontalScrollBar = ({
   setSelectedBodyPart,
 }) => {
   return (
-    <div className="horizontalScrollBar">
-      {data.map((item) => (
-        <BodyPart
-          key={item}
-          part={item}
-          setSelectedBodyPart={setSelectedBodyPart}
-          selectedBodyPart={selectedBodyPart}
-        />
-      ))}
+    <div className="horMenuContainer">
+      <img src={LeftArrow} className="leftArrow" />
+      <ScrollMenu className="horizontalScrollBar">
+        {data.map((item) => (
+          <BodyPart
+            key={item}
+            part={item}
+            setSelectedBodyPart={setSelectedBodyPart}
+            selectedBodyPart={selectedBodyPart}
+          />
+        ))}
+      </ScrollMenu>
+      <img src={rightArrow} className="rightArrow" />
     </div>
   );
 };
