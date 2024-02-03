23d6d8d62f5b12436deb2095f31c27e2c48ef90b
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-02-03 10:04:41 -0800

    Created Exercise box to display exercises

---
+++ src/components/ExerciseBox.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExerciseBox = ({ exercise }) => {
+  return <div className="exerciseBox">{exercise.name}</div>;
+};
+
+export default ExerciseBox;
--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import ExerciseBox from "./ExerciseBox";
 
 const Exercises = ({ exercises, setExercises, selectedBodyPart }) => {
   return (
     <div className="exercisesComponent">
       <h2>Showing Results</h2>
-      <div>Exercises</div>
+      <div>
+        {exercises.map((exercise) => {
+          return <ExerciseBox key={exercise.id} exercise={exercise} />;
+        })}
+      </div>
     </div>
   );
 };
