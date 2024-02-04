51a551a2d6029707b9814a34bc16dff18aad63dd
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-02-03 22:58:37 -0800

    Added styling to the ExerciseBox
    The ExerciseBox component displays the name and muscle groups involved in the exercises displayed in results

--- src/components/ExerciseBox.jsx
+++ src/components/ExerciseBox.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const ExerciseBox = ({ exercise }) => {
-  return <div className="exerciseBox">{exercise.name}</div>;
+  return (
+    <div className="exerciseBox">
+      <img src={exercise.gifUrl} alt={`${exercise.name} gif`} />
+      <div className="exerciseInfoContainer">
+        <div className="musclesInvolved">
+          {exercise.secondaryMuscles.map((muscle) => {
+            return <p>{muscle.charAt(0).toUpperCase() + muscle.slice(1)}</p>;
+          })}
+        </div>
+        <p className="exerciseName">
+          {exercise.name.charAt(0).toUpperCase() + exercise.name.slice(1)}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ExerciseBox;
