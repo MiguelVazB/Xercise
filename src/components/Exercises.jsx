96e9814bfc02dd3c41413acf9b7412397f104428
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-02-05 14:56:33 -0800

    Added pagination for the results

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,16 +1,60 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseBox from "./ExerciseBox";
+import ReactPaginate from "react-paginate";
 
 const Exercises = ({ exercises, setExercises, selectedBodyPart }) => {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const exercisesPerPage = 6;
+  const pageCount = Math.ceil(exercises.length / exercisesPerPage);
+  const pagesVisited = pageNumber * exercisesPerPage;
+
+  const displayExercises = exercises
+    .slice(pagesVisited, pagesVisited + exercisesPerPage)
+    .map((exercise) => {
+      return <ExerciseBox key={exercise.id} exercise={exercise} />;
+    });
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  useEffect(() => {
+    const fetchBodyParts = async () => {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises?limit=-1",
+        exerciseOptions
+      );
+
+      setExercises(exercisesData);
+    };
+
+    fetchBodyParts();
+    console.log("hi");
+  }, []);
+
   return (
     <div className="exercisesComponent">
       <h2>Showing Results</h2>
-      <div>
-        {exercises.map((exercise) => {
+      <div className="exercisesContainer">
+        {/* {exercises.map((exercise) => {
           return <ExerciseBox key={exercise.id} exercise={exercise} />;
-        })}
+        })} */}
+        {displayExercises}
       </div>
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName="paginationButtons"
+        previousLinkClassName="previousButton"
+        nextLinkClassName="nextButton"
+        activeClassName="paginationActive"
+        disabledClassName="disabledButtons"
+      />
     </div>
   );
 };
