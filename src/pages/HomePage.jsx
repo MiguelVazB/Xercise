ff482a4f2625ac98dc44d068194fd32a9c9a0062
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-02-05 16:30:16 -0800

    Created resultsRef and passed it to Exercises and ExerciseSearch
    This ref is used to scroll to that element when the fetching of the search is done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import HeroComponent from "../components/HeroComponent";
 import ExerciseSearch from "../components/ExerciseSearch";
 import "./HomePage.css";
+import Exercises from "../components/Exercises";
 
 const HomePage = () => {
   const [exercises, setExercises] = useState([]);
-  const [selectedBodyPart, setSelectedBodyPart] = useState("");
+  const [selectedBodyPart, setSelectedBodyPart] = useState("all");
+  const resultsRef = useRef(null);
+
+  useEffect(() => {
+    console.log(exercises);
+    console.log(selectedBodyPart);
+  }, [exercises, selectedBodyPart]);
 
   return (
     <main className="homePage">
@@ -15,6 +22,13 @@ const HomePage = () => {
         setExercises={setExercises}
         setSelectedBodyPart={setSelectedBodyPart}
         selectedBodyPart={selectedBodyPart}
+        resultsRef={resultsRef}
+      />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        selectedBodyPart={selectedBodyPart}
+        resultsRef={resultsRef}
       />
     </main>
   );
