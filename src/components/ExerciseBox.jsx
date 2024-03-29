c2bc09c958b39cfb1dc47c8eacab32a28c3ab47e
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-03-29 03:56:29 -0700

    Added states and animations to the exercises

--- src/components/ExerciseBox.jsx
+++ src/components/ExerciseBox.jsx
@@ -1,30 +1,62 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import { motion, LayoutGroup, AnimatePresence } from "framer-motion";
 
 const ExerciseBox = ({ exercise }) => {
+  const [loaded, setLoaded] = useState(false);
+  const [imageLoading, setImageLoading] = useState(true);
+  const [pulsing, setPulsing] = useState(true);
+
+  useEffect(() => {
+    if (loaded) {
+      setImageLoading(false);
+      setPulsing(false);
+    }
+  }, [loaded]);
+
   return (
-    <div className="exerciseBox">
-      <div className="imageContainer">
-        <img
-          src={exercise.gifUrl}
-          alt={`${exercise.name} gif`}
-          loading="lazy"
-        />
-      </div>
-      <div className="exerciseInfoContainer">
-        <div className="musclesInvolved">
-          {exercise.secondaryMuscles.map((muscle) => {
-            return (
-              <p key={`${muscle}${exercise.id}`}>
-                {muscle.charAt(0).toUpperCase() + muscle.slice(1)}
-              </p>
-            );
-          })}
+    <AnimatePresence>
+      <motion.div
+        key="modal"
+        className="exerciseBox"
+        initial={{ x: -500, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -500, opacity: 0 }}
+        transition={{ duration: 0.3, delay: 0.2 }}
+      >
+        {/* <div className="exerciseBox"> */}
+        <div className={`imageContainer ${pulsing ? "pulse" : ""}`}>
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: imageLoading ? 0 : 1,
+            }}
+            transition={{
+              opacity: { delay: 0, duration: 0.2 },
+            }}
+            style={loaded ? {} : { display: "none" }}
+            onLoad={() => setLoaded(true)}
+            src={exercise.gifUrl}
+            alt={`${exercise.name} gif`}
+            // loading="lazy"
+          />
         </div>
-        <p className="exerciseName">
-          {exercise.name.charAt(0).toUpperCase() + exercise.name.slice(1)}
-        </p>
-      </div>
-    </div>
+        <div className="exerciseInfoContainer">
+          <div className="musclesInvolved">
+            {exercise.secondaryMuscles.map((muscle) => {
+              return (
+                <p key={`${muscle}${exercise.id}`}>
+                  {muscle.charAt(0).toUpperCase() + muscle.slice(1)}
+                </p>
+              );
+            })}
+          </div>
+          <p className="exerciseName">
+            {exercise.name.charAt(0).toUpperCase() + exercise.name.slice(1)}
+          </p>
+        </div>
+        {/* </div> */}
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
