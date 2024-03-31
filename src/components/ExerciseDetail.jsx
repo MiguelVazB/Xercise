db509ab5f9b1986de340c75585e58c84e3a1e40e
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-03-31 16:11:55 -0700

    Added logic to make the ExerciseDetail component work
    if  the user got to the page by pressing  the ExerciseBox, it'll send the data over so that the user does not unnecessarily call the API. If the user gets to the page by entering a specific URL then the API will be called

--- src/components/ExerciseDetail.jsx
+++ src/components/ExerciseDetail.jsx
@@ -1,35 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const ExerciseDetail = ({ exercise }) => {
-  const { id, name, gifUrl, instructions, target, secondaryMuscles } = exercise;
+  const [exerciseState, setExerciseState] = useState({
+    id: null,
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (exercise != null) {
+      setExerciseState(exercise);
+      console.log("loaded from localstorage");
+    } else {
+      const fetchExercise = async () => {
+        let exerciseFetched = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`,
+          exerciseOptions
+        );
+        setExerciseState(exerciseFetched);
+      };
+      fetchExercise();
+      console.log("fetched exercise from id");
+    }
+  }, []);
+
   return (
     <div className="exerciseDetailComponent">
-      <div className="infoAndPic">
-        <div className="exerciseImgContainer">
-          <img src={gifUrl} alt="exercise image" />
-        </div>
-      </div>
-      <div className="detailsContainer">
-        <h1>{name.charAt(0).toUpperCase() + name.slice(1)}</h1>
-        <h2>Instructions</h2>
-        <div>
-          {instructions.map((step) => (
-            <p>{step}</p>
-          ))}
-        </div>
-        <div>
-          <h3>Muscles involved</h3>
-          <ul>
-            <li>{target}</li>
-          </ul>
-          <h4>Secondary Muscles</h4>
-          <ul>
-            {secondaryMuscles.map((muscle) => (
-              <li key={muscle}>{muscle}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      {exerciseState.id && (
+        <>
+          <div className="exercisePic">
+            <div className="exerciseImgContainer">
+              <img src={exerciseState.gifUrl} alt="exercise image" />
+            </div>
+          </div>
+          <div className="detailsContainer">
+            <h1>
+              {exerciseState.name?.charAt(0).toUpperCase() +
+                exerciseState.name?.slice(1)}
+            </h1>
+            <h2>Instructions</h2>
+            <div>
+              {exerciseState.instructions?.map((step, index) => (
+                <p key={index}>{step}</p>
+              ))}
+            </div>
+            <div className="musclesUsed">
+              <h3>Muscles involved</h3>
+              <ul>
+                <li>{exerciseState.target}</li>
+              </ul>
+              <h4>Secondary Muscles</h4>
+              <ul>
+                {exerciseState.secondaryMuscles?.map((muscle) => (
+                  <li key={muscle}>{muscle}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
