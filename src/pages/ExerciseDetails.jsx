f41af3cbaa88386bbaf6c30b9dc53af65091def9
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-03-31 01:39:32 -0700

    Created ExercisesDetails
    Created the initial structure of the page

---
+++ src/pages/ExerciseDetails.jsx
@@ -0,0 +1,15 @@
+import { React, useEffect, useState } from "react";
+import "./ExerciseDetails.css";
+
+const ExerciseDetails = () => {
+  return (
+    <div className="exerciseDetails">
+      <div>ExerciseDetails</div>
+      <div>Exercise videos</div>
+      <div>Similar exercise target</div>
+      <div>Similar exercise equipment</div>
+    </div>
+  );
+};
+
+export default ExerciseDetails;
