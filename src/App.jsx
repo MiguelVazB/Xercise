ec99c7df16cbc8d3e5fdf81348c0273b854ce1c3
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-03-31 01:39:01 -0700

    Added exercises path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
+import ExerciseDetails from "./pages/ExerciseDetails";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/exercises" element={<ExerciseDetails />} />
       </Routes>
     </>
   );
