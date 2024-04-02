27041e2c36b09f3d053c8877aa921aea4839d984
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-01 20:43:58 -0700

    Created ExerciseVideos component to use in the HorizontalScrollBar component

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import HorizontalScrollBar from "./HorizontalScrollBar";
 
 const ExerciseVideos = ({ exercise, exerciseId }) => {
-  return <div>ExerciseVideos</div>;
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  return (
+    <HorizontalScrollBar
+      componentToDisplay="exerciseVideos"
+      data={["hi", "hi", "hi", "hi", "hi"]}
+      selectedItem={selectedItem}
+      setSelectedItem={setSelectedItem}
+    />
+  );
 };
 
 export default ExerciseVideos;
