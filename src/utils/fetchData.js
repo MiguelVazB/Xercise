68fd387aa46f0c8a9c262804586ea2d8445b5d27
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-02 19:46:30 -0700

    Added videoOptions and fixed logic
    Added videoOptions to call the Youtube V2 API. I fexd the fecthData function. I return null if the id provided does not exist in the API

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,12 +2,31 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
         'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     }
 }
 
 export const fetchData = async(url, options) => {
 
+    const response = await fetch(url, options);
+
+    try {
+        const data = await response.json()
+        return data
+    } catch (error) {
+        return null
+    }
+}
+
+export const videoOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-v2.p.rapidapi.com'
+    }
+}
+
+export const fetchVideoData = async(url, options) => {
     const response = await fetch(url, options);
 
     const data = await response.json()
