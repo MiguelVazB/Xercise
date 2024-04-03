62455a17240b4365101eedf209ea22c5dec63238
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-02 19:53:03 -0700

    Update VideoComponent.jsx
    Finished implementing the structure of each VideoComponent.

--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 
 const VideoComponent = ({ video, selectedItem, setSelectedItem }) => {
+  const formatViews = (views) => {
+    return Number(views).toLocaleString();
+  };
+
   return (
-    <div onClick={() => setSelectedItem(video)}>
-      <p>{video}</p>
+    <div
+      className={`videoComponentContainer ${
+        video == selectedItem ? "selectedContainer" : ""
+      }`}
+      onClick={() => {
+        setSelectedItem(video.title);
+        window.open(
+          `https://www.youtube.com/watch?v=${video.video_id}`,
+          "_blank"
+        );
+        // .focus();
+      }}
+    >
+      <div className="thumbnail">
+        <img
+          className="thumbnailImg"
+          alt={`${video?.title} video`}
+          src={video.thumbnails[0].url}
+        />
+        <div className="length">{video.video_length}</div>
+      </div>
+      <div className="videoInfo">
+        <p className="videoTitle">{video?.title}</p>
+        <div className="channelAndViewsContainer">
+          <p className="videoChannel">{video?.author}</p>
+          <p className="videoViews">
+            {formatViews(video.number_of_views)} views
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
