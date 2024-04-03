fe367eba6897fff367e7ec09aa9dc32753440fee
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-02 19:46:59 -0700

    Moved the fetch function to the parent page

--- src/components/ExerciseDetail.jsx
+++ src/components/ExerciseDetail.jsx
@@ -1,54 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-const ExerciseDetail = ({ exercise, exerciseId }) => {
-  const [exerciseState, setExerciseState] = useState({
-    id: null,
-  });
-
-  useEffect(() => {
-    if (exercise != null) {
-      setExerciseState(exercise);
-      console.log("loaded from localstorage");
-    } else {
-      const fetchExercise = async () => {
-        let exerciseFetched = await fetchData(
-          `https://exercisedb.p.rapidapi.com/exercises/exercise/${exerciseId}`,
-          exerciseOptions
-        );
-        setExerciseState(exerciseFetched);
-      };
-      fetchExercise();
-      console.log("fetched exercise from id");
-    }
-  }, []);
-
+const ExerciseDetail = ({ exercise }) => {
   return (
     <div className="exerciseDetailComponent">
-      {exerciseState.id && (
+      {exercise?.id && (
         <>
           <div className="exerciseImgContainer">
-            <img src={exerciseState.gifUrl} alt="exercise image" />
+            <img src={exercise.gifUrl} alt="exercise image" />
           </div>
           <div className="detailsContainer">
             <h1>
-              {exerciseState.name?.charAt(0).toUpperCase() +
-                exerciseState.name?.slice(1)}
+              {exercise.name?.charAt(0).toUpperCase() + exercise.name?.slice(1)}
             </h1>
             <h2>Instructions</h2>
             <div>
-              {exerciseState.instructions?.map((step, index) => (
+              {exercise.instructions?.map((step, index) => (
                 <p key={index}>{step}</p>
               ))}
             </div>
             <div className="musclesUsed">
               <h3>Muscles involved</h3>
               <ul>
-                <li>{exerciseState.target}</li>
+                <li>{exercise.target}</li>
               </ul>
               <h4>Secondary Muscles</h4>
               <ul>
-                {exerciseState.secondaryMuscles?.map((muscle) => (
+                {exercise.secondaryMuscles?.map((muscle) => (
                   <li key={muscle}>{muscle}</li>
                 ))}
               </ul>
