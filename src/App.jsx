3b4b4eba6f8861e895dece38d6c150258cc0bc1a
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-02 23:38:38 -0700

    Added Suspense to Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
+import { lazy, Suspense } from "react";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
-import ExerciseDetailsPage from "./pages/ExerciseDetailsPage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
+const ExerciseDetailsPage = lazy(() => import("./pages/ExerciseDetailsPage"));
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="exercises" element={<ExerciseDetailsPage />}>
-          <Route path=":id" element={<ExerciseDetailsPage />} />
-        </Route>
-        <Route path="*" element={<div>Not found</div>} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="exercises" element={<ExerciseDetailsPage />}>
+            <Route path=":id" element={<ExerciseDetailsPage />} />
+          </Route>
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
