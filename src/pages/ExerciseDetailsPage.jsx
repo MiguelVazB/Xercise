baeefd2f9cdf9ebd447e2513b14de17bace2d66b
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-02 19:51:33 -0700

    Update ExerciseDetailsPage.jsx
    Added logic to fetch the exercise. If the exercise is passed through location.state then I would use that info. If the user enters through the url, it'll fetch the exercise.
    Also, I'm saving the exercise and the videos for that exercise in sessionStorage so that the user does not call the API unnecessarily.

--- src/pages/ExerciseDetailsPage.jsx
+++ src/pages/ExerciseDetailsPage.jsx
@@ -1,13 +1,49 @@
 import { React, useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
-import "./ExerciseDetailsPage.css";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseDetail from "../components/ExerciseDetail";
 import ExerciseVideos from "../components/ExerciseVideos";
+import "./ExerciseDetailsPage.css";
 
 const ExerciseDetails = () => {
   const location = useLocation();
-  const exercise = location.state;
+  const exerciseFromState = location.state;
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [exercise, setExercise] = useState(null);
+
+  useEffect(() => {
+    if (exerciseFromState != null) {
+      setExercise(exerciseFromState);
+      console.log("loaded from localstorage");
+    } else {
+      const fetchExercise = async () => {
+        let exerciseFetched = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`,
+          exerciseOptions
+        );
+
+        if (exerciseFetched == null) navigate("/*");
+
+        sessionStorage.setItem(
+          `exerciseID_${id}`,
+          JSON.stringify(exerciseFetched)
+        );
+        setExercise(exerciseFetched);
+      };
+
+      let inSession = sessionStorage.getItem(`exerciseID_${id}`);
+      if (inSession === "undefined") {
+        console.log("fetched exercise from id");
+        fetchExercise();
+      } else {
+        console.log("loaded from session");
+        if (inSession == null) navigate("/*");
+        setExercise(JSON.parse(inSession));
+      }
+    }
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0, { behavior: "smooth" });
@@ -15,10 +51,16 @@ const ExerciseDetails = () => {
 
   return (
     <div className="exerciseDetailsPage">
-      <ExerciseDetail exercise={exercise} exerciseId={id} />
-      <ExerciseVideos exercise={exercise} exerciseId={id} />
-      <div>Similar exercise target</div>
-      <div style={{ height: "100vh" }}>Similar exercise equipment</div>
+      {exercise != null ? (
+        <>
+          <ExerciseDetail exercise={exercise} />
+          <ExerciseVideos exercise={exercise} />
+          <div>Similar exercise target</div>
+          <div style={{ height: "100vh" }}>Similar exercise equipment</div>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
