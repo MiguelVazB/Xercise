bb534590e08cfb0de62b6a397a33aa0b7dc7658c
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-03 01:43:57 -0700

    Update ExerciseVideos.jsx

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -3,18 +3,18 @@ import { useState, useEffect } from "react";
 import HorizontalScrollBar from "./HorizontalScrollBar";
 import { videoOptions, fetchVideoData } from "../utils/fetchData";
 
-const ExerciseVideos = ({ exercise }) => {
+const ExerciseVideos = ({ exerciseName }) => {
   const [selectedItem, setSelectedItem] = useState("");
   const [videoData, setVideoData] = useState([]);
 
   const getVideosData = async () => {
-    if (exercise.name) {
+    if (exerciseName) {
       const videoData = await fetchVideoData(
-        `https://youtube-v2.p.rapidapi.com/search/?query=${`${exercise.name} exercise`}&lang=en`,
+        `https://youtube-v2.p.rapidapi.com/search/?query=${`${exerciseName} exercise`}&lang=en`,
         videoOptions
       );
       sessionStorage.setItem(
-        `${exercise.name}_d`,
+        `${exerciseName}_d`,
         JSON.stringify(videoData.videos)
       );
       setVideoData(videoData.videos);
@@ -22,8 +22,8 @@ const ExerciseVideos = ({ exercise }) => {
   };
 
   useEffect(() => {
-    if (exercise?.name) {
-      let inSession = sessionStorage.getItem(`${exercise?.name}_d`);
+    if (exerciseName) {
+      let inSession = sessionStorage.getItem(`${exerciseName}_d`);
       if (inSession != null) {
         setVideoData(JSON.parse(inSession));
         console.log("loaded videos from session");
@@ -32,11 +32,11 @@ const ExerciseVideos = ({ exercise }) => {
         console.log("fetched videos");
       }
     }
-  }, [exercise]);
+  }, [exerciseName]);
 
   return (
     <div className="exerciseVideos">
-      <div className="videosHeading">Videos related to {exercise?.name}</div>
+      <div className="scrollHeading">Videos related to {exerciseName}</div>
       <div className="scrollBarContainer">
         <HorizontalScrollBar
           componentToDisplay="exerciseVideos"
