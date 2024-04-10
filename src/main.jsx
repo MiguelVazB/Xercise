e92f998264e7b4e093ba89f1d894fb6cf2604c57
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-04-10 01:49:04 -0700

    Fixing routing issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
